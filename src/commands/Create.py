3934df5d72719d816819661179d41beb103dd6c5
Hiago75 <hiagobrenha75@gmail.com>
2022-04-22 13:40:22 -0300

    feat: add user messages

--- src/commands/Create.py
+++ src/commands/Create.py
@@ -8,7 +8,10 @@ current_repository = os.getcwd()
 @click.command()
 @click.option('--repo_name', prompt="Project name", required=True)
 @click.option('--repo_url', prompt="Github repo URL", required=True)
-@click.option('--repo_dir', prompt="Project directory", required=True, default=current_repository)
+@click.option('--repo_dir', prompt="Project directory", default=current_repository)
 def create(repo_name,repo_url,repo_dir): 
   git_handler = GitHandler(repo_name,repo_url, repo_dir)
+
+  click.secho('Cloning repo', fg="bright_blue")
   git_handler.clone_repo()
+  click.secho('Repo cloned', fg="green")
